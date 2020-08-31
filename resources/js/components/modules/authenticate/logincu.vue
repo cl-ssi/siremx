640b5b5ce8c0c72b08060aa1689b69ac3b44fb8e
jcortesf <juliocortesf@gmail.com>
2020-08-30 23:39:29 -0400

    fix: userToken in logincu component

--- resources/js/components/modules/authenticate/logincu.vue
+++ resources/js/components/modules/authenticate/logincu.vue
@@ -23,7 +23,7 @@
             login() {
               
                 this.fullscreenLoading = true;
-                var  url = '/authenticate/logincu/'+userToken
+                var  url = '/authenticate/logincu/'+this.userToken
                 axios.get(url).then(response => {
                     if(response.data.code == 401){
                         this.loginFailed();
