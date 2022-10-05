b6c7d9f6bc3d6bfb36e8e02dec12325871ed2eb9
Esteban Miranda <tebiccr@gmail.com>
2022-10-05 18:04:53 -0300

    attempt 1 logout CU

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -12,9 +12,9 @@ use App\User;
 class LoginController extends Controller
 {
 
-    protected $maxAttempts = 3; 
-    protected $decayMinutes = 2; 
-    
+    protected $maxAttempts = 3;
+    protected $decayMinutes = 2;
+
     public function login(Request $request)
     {
 
@@ -22,13 +22,12 @@ class LoginController extends Controller
         $password = $request->pass;
         $resp     = Auth::attempt(['username' => $email, 'password' => $password, 'state' => 'A']);
 
-        if($resp) {
+        if ($resp) {
             return response()->json([
                 'authUser' => Auth::user(),
                 'code'     => 200
             ]);
-        }
-        else {
+        } else {
             return response()->json([
                 'code'     => 401
             ]);
@@ -47,35 +46,40 @@ class LoginController extends Controller
     public function logincu($access_token = null)
     {
         if ($access_token) {
-			/* Paso 3, obtener los datos del usuario en base al $access_token */
-			$url_base = "https://accounts.claveunica.gob.cl/openid/userinfo/";
-			$response = Http::withToken($access_token)->post($url_base);
-			$user_cu = json_decode($response);
-	
-			$u = User::where('run',$user_cu->RolUnico->numero)->first();
-			
-			if($u) {
-				Auth::login($u, true);
-				$resp = auth()->id();
-				if($resp) {
-					return response()->json([
-						'authUser' => Auth::user(),
-						'code'     => 200
-						
-					]);
-				}
-				else {
-					return response()->json([
-						'code'     => 401
-					]);
-				}
-			}
-			else {
-				return response()->json([
-					'code'     => 401
-				]);
-			}
-    
+            /* Paso 3, obtener los datos del usuario en base al $access_token */
+            $url_base = "https://accounts.claveunica.gob.cl/openid/userinfo/";
+            $response = Http::withToken($access_token)->post($url_base);
+            $user_cu = json_decode($response);
+
+            $u = User::where('run', $user_cu->RolUnico->numero)->first();
+
+            if ($u) {
+                Auth::login($u, true);
+                $resp = auth()->id();
+                if ($resp) {
+                    return response()->json([
+                        'authUser' => Auth::user(),
+                        'code'     => 200
+
+                    ]);
+                } else {
+                    return response()->json([
+                        'code'     => 401
+                    ]);
+                }
+            } else {
+                /** Cerrar sesión clave única */
+                /* Url para cerrar sesión en clave única */
+                $url_logout     = "https://accounts.claveunica.gob.cl/api/v1/accounts/app/logout?redirect=";
+                /* Url para luego cerrar sesión en nuestro sisetema */
+                $url_redirect   = env('APP_URL') . "/logout";
+                $url            = $url_logout . urlencode($url_redirect);
+                session()->flash('danger', 'Esta cuenta no coincide con nuestros registros');
+                return redirect($url);
+                // return response()->json([
+                //     'code'     => 401
+                // ]);
+            }
         }
     }
 }
